fa8db587e458ef0dd351517ed3e39b40985f4056
de la Mora Ocampo external <iris.delamoraocampoexternal@capgemini.com>
2024-11-25 11:34:21 +0100

    listado simple del proyecto ludoteca con spring boot

---
+++ src/main/java/com/ccsw/tutorial/category/CategoryServiceImpl.java
@@ -0,0 +1,63 @@
+package com.ccsw.tutorial.category;
+
+import com.ccsw.tutorial.category.model.Category;
+import com.ccsw.tutorial.category.model.CategoryDto;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+    //     private long SEQUENCE = 1;
+    //    private Map<Long, CategoryDto> categories = new HashMap<Long, CategoryDto>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Category> findAll() {
+
+        // return new ArrayList<CategoryDto>(this.categories.values());
+        return (List<Category>) this.categoryRepository.findAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void save(Long id, CategoryDto dto) {
+        // CategoryDto category;
+        Category category;
+
+        if (id == null) {
+            // category = new CategoryDto();
+            // category.setId(this.SEQUENCE++);
+            category = new Category();
+        } else {
+            // category = this.categories.get(id);
+            category = this.categoryRepository.findById(id).orElse(null);
+        }
+
+        category.setName(dto.getName());
+        this.categoryRepository.save(category);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void delete(Long id) throws Exception {
+
+        // this.categories.remove(id);
+
+        if (this.categoryRepository.findById(id).orElse(null) == null) {
+            throw new Exception("Not exists");
+        }
+
+        this.categoryRepository.deleteById(id);
+    }
+
+}
